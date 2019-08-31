d01473d7a3b2e9610f1be9e27ba143d16a8a5b57
duc.nt <ittrongduc@gmail.com>
2019-09-01 02:18:21 +0700

    [ProductManager] init

---
+++ src/main/java/com/zdzz/product/controller/AppController.java
@@ -0,0 +1,28 @@
+package com.zdzz.product.controller;
+
+import com.zdzz.product.model.Product;
+import com.zdzz.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping("/")
+    public String viewHomePage(Model model){
+
+        List<Product> productList = productService.listAll();
+
+        model.addAttribute("productList", productList);
+
+        return "index";
+    }
+
+}
